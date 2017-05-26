4fc43802830d342af8603d452112431bf95fbe10
AlexKlv <alexcoolgandy@mail.ru>
2017-05-26 20:55:41 +0300

    Add Server and all task for T3

---
+++ PSSIP-Project/PSSIP-Project/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSSIP_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllImages()
+        {
+            var test = new List<string>();
+            return Json(test, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
